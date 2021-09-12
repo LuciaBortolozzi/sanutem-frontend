32625d73cca93fc1b464ac76115e9ecca1c5aaa2
Lucia Bortolozzi <luciabortolozzi@gmail.com>
2021-09-12 01:28:26 -0300

    Search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -9,6 +10,7 @@ import {ActivatedRoute} from '@angular/router';
 export class SearchComponent implements OnInit {
   private name: string;
   focus1: boolean;
+  searchForm: FormGroup;
 
   constructor(private activatedRoute: ActivatedRoute) {
     this.name = this.activatedRoute.snapshot.params.name;
@@ -17,4 +19,11 @@ export class SearchComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  search() {
+
+  }
+
+  getSelectedDropdown(s: string) {
+    console.log(s);
+  }
 }
